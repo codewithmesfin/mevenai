c52956a8afb351cfebbb41a250783534afb46d8b
System Administrator <sciemesfin55@gmail.com>
2024-07-13 13:52:54 +0300

    Update the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 import OnboardingSection from "@/components/onboardSection";
 import TypewriterEffect from "@/components/typeWriterEffect";
-import { ChevronRightIcon, Cog8ToothIcon, FolderOpenIcon } from "@heroicons/react/24/outline";
+import { cards, services } from "@/data/services";
+import { ArrowTopRightOnSquareIcon, ChevronRightIcon, Cog8ToothIcon, FolderOpenIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 
@@ -87,15 +88,76 @@ export default function Home() {
 
 
       <section>
-        <div className="w-[90%] md:w-[80%] mx-auto py-16">
-          <h1 style={{
-            lineHeight: 1.3
-          }} className="text-4xl md:text-7xl font-bold text-center">
-            Helping businesses and individuals to
-            find the best fitting software.
-          </h1>
+        <div className="w-[90%] mx-auto py-16">
+          <div className="md:flex items-center md:space-x-10">
+            <div className="w-full md:w-2/5 pb-10 md:pb-1">
+              <h1
+                style={{ lineHeight: 1.3 }}
+                className="font-extrabold text-4xl md:text-5xl"
+              >
+                {"We're"} helping <br />
+                <span
+                  className="bg-gradient-to-r from-[#007aff] to-green-600 inline-block text-transparent bg-clip-text"
+                >
+                  Individuals
+                </span> and
+                <br />
+                <span
+                  className="bg-gradient-to-r from-green-600 to-[#007aff] inline-block text-transparent bg-clip-text"
+                >
+                  Businesses
+                </span>
+              </h1>
+            </div>
+            <div className="w-full md:w-3/5 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-10">
+              {cards.map((item: any) => (
+                <Link key={item.title} href={"item.href"}>
+                  <div
+                    className="rounded-[20px] p-5 border hover:border-4 hover:border-green-50 cursor-pointer bg-gradient-to-r hover:from-green-200 hover:to-green-100"
+                  >
+                    <div className="flex items-center justify-between space-x-3 md:space-x-10">
+                      <h1 className="text-xl py-3 font-semibold text-gray-900">
+                        {item.title}
+                      </h1>
+                      <span>
+                        <ArrowTopRightOnSquareIcon path="right" className="h-4 w-4" strokeWidth={2} />
+                      </span>
+                    </div>
+                    <p className="text-[#333333]">
+                      {item.subtitle}
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="bg-white">
+        <div className="w-[95%] md:w-[90%] mx-auto py-16">
+        <h1
+                style={{lineHeight:1.3}}
+                className="font-semibold text-center text-5xl md:text-7xl"
+              >
+                We make your <br />
+                business a
+                <span
+                  className="border-b-4 text-[#007aff] border-gray-200 pb-2 font-extrabold"
+                  >different!</span
+                >
+              </h1>
           <div className="py-10">
-            {/* asdfgh */}
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-6">
+              {
+                services.map((item: any, i: number) => <div key={item.title}>
+                  <div className="border border-gray-100 p-5">
+                    sadfg
+                  </div>
+                </div>)
+              }
+            </div>
           </div>
         </div>
       </section>
--- data/services.ts
+++ data/services.ts
@@ -1,61 +1,36 @@
- const services: any = [
-        {
-            title: "Manage tech team",
-            subtitle: "Remote or on-site, managing a tech team can be complex. I can guide you through the process to create a strong, efficient team that drives success.",
-            href: "/manage-team",
-            free: true,
-            type: "Agile",
-            banner: "https://img.freepik.com/free-photo/team-working-together-project_23-2149325411.jpg"
-        },
-        {
-            title: "Manage tech products",
-            subtitle: "Whether you manage a remote or on-site team, I offer expert guidance to optimize your software development process and create client-favorite products",
-            href: "/asdf",
-            type: "Agile",
-            banner: "https://coamplifi.com/wp-content/uploads/2022/02/coamplifi_10pmprinciples-750x550.jpg"
-        },
-        {
-            title: "Run ERPNext for free",
-            subtitle: "Allow me to show you how can you automate and streamline your business operations within the world's 100% free ERP.",
-            type: "Open Source",
-            href: "/asdf",
-            banner: "https://i0.wp.com/lemonlearning.com/wp-content/uploads/2023/01/ERP-1-scaled.jpg?fit=2560%2C1707&ssl=1"
-        },
-        {
-            title: "Document projects",
-            subtitle: "Strong documentation can save time and headaches down the line. Have you considered some different approaches for your project?",
-            type: "Standards",
-            href: "/asdf",
-            banner: "https://www.mindmeister.com/blog/wp-content/uploads/2019/03/Document-Writing-1024x593.png"
-        },
-        {
-            title: "Develop Softwares",
-            subtitle: "Need help building your dream software? I can provide personalized advice based on your specific needs.",
-            type: "Standards",
-            href: "/asdf",
-            banner: "https://www.iss.nus.edu.sg/images/default-source/default-album/software-dev.jpg?Status=Master&sfvrsn=aa8aa05f_0"
-        },
-        {
-            title: "Do DevOps",
-            subtitle: "Reduce bottlenecks and improve project flow with continuous integration and deployment.",
-            type: "Standards",
-            href: "/asdf",
-            banner: "https://vivasoftltd.com/wp-content/uploads/2023/10/Advantages-and-Disadvantages-of-DevOps.jpg"
-        },
-        {
-            title: "Deal with Blockchain",
-            subtitle: "Security, ownership, and trust are key concerns today. Decentralized development offers solutions! Want to see how?",
-            type: "Standards",
-            href: "/asdf",
-            banner: "https://vivasoftltd.com/wp-content/uploads/2023/10/Advantages-and-Disadvantages-of-DevOps.jpg"
-        },
-        {
-            title: "Leaverage with Gen AI",
-            subtitle: "Smart technology is at your fingertips! Let's develop your ideal automated and personalized system together, in a way that fits your budget.",
-            type: "Standards",
-            href: "/asdf",
-            banner: "https://vivasoftltd.com/wp-content/uploads/2023/10/Advantages-and-Disadvantages-of-DevOps.jpg"
-        }
-    ]
+ const services: any =[
+    {
+        img:"",
+        title:"ERPNext",
+        subtitle:"",
+        availability:"Free"
+    },
+    {
+        img:"",
+        title:"Generative AI",
+        subtitle:"",
+        availability:"Free"
+    },
+    {
+        img:"",
+        title:"Odoo",
+        subtitle:"",
+        availability:"Free"
+    }
+ ]
 
-    export {services}
+ const cards = [
+    {
+      title: "Identify the right Solution",
+      subtitle:
+        "We have several years of expereince on the tech industry. If you are worring on choosing which Software or technology works best for you, feel free to reach us.",
+      href:"/contact"
+      },
+    {
+      title: "Setup a perfect team",
+      subtitle:"Whether you are running a remote or on-site tech team and you dought in productivity, just meet our team. We can help you ending up with the right team.",
+      href:"/learn"
+    },
+  ];
+
+    export {services,cards}
