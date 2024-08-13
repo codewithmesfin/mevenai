b5a0c21d8fbe7fff1e5b7d8f60596919b46e5353
System Administrator <sciemesfin55@gmail.com>
2024-08-13 17:15:34 +0300

    Update homescreen responsiveness

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
       <section
         className="bg-[#0a0a0a] flex items-center">
-        <div className="w-[90%] md:w-[80%] mx-auto py-48">
+        <div className="w-[95%] md:w-[80%] mx-auto py-32 md:py-48">
           <div className="">
             <div className="w-full">
               <div className="my-3 py-2 rounded-xl border border-gray-800 mx-auto w-[90%] md:max-w-[400px]">
@@ -24,13 +24,13 @@ export default function Home() {
                   <span className="text-gray-300">
                     <CheckBadgeIcon className="w-5 h-5" />
                   </span>
-                  <h1 className="text-gray-400 md:text-lg text-center">Amazing Resources Coming Your Way!</h1>
+                  <h1 className="text-gray-400 text-sm md:text-lg text-center">Amazing Resources Coming Your Way!</h1>
                 </div>
               </div>
               <div className="py-4">
                 <h1
                   style={{ lineHeight: 1.2 }}
-                  className="text-4xl md:text-6xl font-extrabold bg-gradient-to-r from-gray-400 to-gray-200 text-center text-transparent bg-clip-text"
+                  className="text-3xl md:text-6xl font-extrabold bg-gradient-to-r from-gray-400 to-gray-200 text-center text-transparent bg-clip-text"
                 >
                   Ready to Revolutionize Your business? Join the Mevinai Waitlist Today!
                 </h1>
