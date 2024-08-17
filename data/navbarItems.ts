a9077f332d3bf045dee1e2c6fafa9faeee8d7523
System Administrator <sciemesfin55@gmail.com>
2024-08-17 11:30:08 +0300

    Update Navbar Items

--- data/navbarItems.ts
+++ data/navbarItems.ts
@@ -2,8 +2,8 @@ const navbarItems: any = [
     { name: "Home", href: "/", current: true },
     { name: "About", href: "/about", current: false },
     { name: "Services", href: "/services", current: false },
-    { name: "Courses", href: "/courses", current: false },
     { name: "How it works", href: "/guide", current: false },
+    { name: "Courses", href: "/courses", current: false },
     // { name: "Articles", href: "/article", current: false },
   ];
   export default navbarItems;
